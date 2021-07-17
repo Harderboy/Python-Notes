565686e383799ab69ccd625e94545ccb7ab9ac31
tomato <37562749+Harderboy@users.noreply.github.com>
2021-07-17 12:02:47 +0800

    Update 2.创建数据库表.py

--- MySQL/MySQL与python交互/2.创建数据库表.py
+++ MySQL/MySQL与python交互/2.创建数据库表.py
@@ -6,7 +6,7 @@ db = pymysql.connect("localhost", "root", "123456", "a")
 cursor = db.cursor()
 
 # 检查表是否存在，存在则删除
-# cursor.execute("drop table if exits bandcard")
+# cursor.execute("drop table if exists bandcard")
 
 # 建表
 sql = "create table bandcard(id int auto_increment primary key, money int not null, name varchar(20) not null)"
